13aa4da7e72108d7df3d90d89bc05cc76197c137
Julliane Galano <jullianegalano@gmail.com>
2025-11-09 20:03:04 +0800

    .

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace PasigLibrarySystem
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new ADMIN.AdminDashboard());
+            Application.Run(new LogIn());
         }
     }
 }
