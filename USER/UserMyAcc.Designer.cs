1ccf446f5d892861f8679dbd22aa93ea7e9ab117
Julliane Galano <jullianegalano@gmail.com>
2025-11-10 20:52:24 +0800

    My Acc halfdone

--- USER/UserMyAcc.Designer.cs
+++ USER/UserMyAcc.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UserMyAcc));
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
             headerpanel = new Panel();
             LogOutbtn = new Button();
             welcomelbl = new Label();
@@ -50,6 +51,17 @@
             panel4 = new Panel();
             pictureBox6 = new PictureBox();
             Settingslbl = new Label();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            dataGridView1 = new DataGridView();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
             headerpanel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             taskbarpanel.SuspendLayout();
@@ -63,6 +75,7 @@
             ((System.ComponentModel.ISupportInitialize)pictureBox9).BeginInit();
             panel4.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
             // headerpanel
@@ -303,12 +316,163 @@
             Settingslbl.TextAlign = ContentAlignment.TopCenter;
             Settingslbl.Click += Settingslbl_Click;
             // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Poppins", 13F);
+            label1.ForeColor = Color.FromArgb(15, 46, 103);
+            label1.Location = new Point(284, 123);
+            label1.Name = "label1";
+            label1.Size = new Size(122, 31);
+            label1.TabIndex = 8;
+            label1.Text = "My Account";
+            label1.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Transparent;
+            label2.Font = new Font("Arial Rounded MT Bold", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.FromArgb(15, 46, 103);
+            label2.Location = new Point(284, 137);
+            label2.Name = "label2";
+            label2.Size = new Size(945, 24);
+            label2.TabIndex = 18;
+            label2.Text = "_____________________________________________________________________________________";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Poppins", 13F);
+            label3.ForeColor = Color.FromArgb(15, 46, 103);
+            label3.Location = new Point(284, 324);
+            label3.Name = "label3";
+            label3.Size = new Size(124, 31);
+            label3.TabIndex = 19;
+            label3.Text = "Book History";
+            label3.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = Color.Transparent;
+            label4.Font = new Font("Arial Rounded MT Bold", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.ForeColor = Color.FromArgb(15, 46, 103);
+            label4.Location = new Point(284, 338);
+            label4.Name = "label4";
+            label4.Size = new Size(945, 24);
+            label4.TabIndex = 20;
+            label4.Text = "_____________________________________________________________________________________";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Poppins", 13F);
+            label5.ForeColor = Color.FromArgb(15, 46, 103);
+            label5.Location = new Point(281, 201);
+            label5.Name = "label5";
+            label5.Size = new Size(69, 31);
+            label5.TabIndex = 21;
+            label5.Text = "Name";
+            label5.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Poppins", 13F);
+            label6.ForeColor = Color.FromArgb(15, 46, 103);
+            label6.Location = new Point(281, 262);
+            label6.Name = "label6";
+            label6.Size = new Size(107, 31);
+            label6.TabIndex = 22;
+            label6.Text = "Username";
+            label6.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Poppins", 13F);
+            label7.ForeColor = Color.FromArgb(15, 46, 103);
+            label7.Location = new Point(822, 201);
+            label7.Name = "label7";
+            label7.Size = new Size(123, 31);
+            label7.TabIndex = 23;
+            label7.Text = "Date Joined";
+            label7.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToDeleteRows = false;
+            dataGridView1.BackgroundColor = Color.White;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = SystemColors.Window;
+            dataGridViewCellStyle1.Font = new Font("Poppins", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            dataGridViewCellStyle1.ForeColor = SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = DataGridViewTriState.False;
+            dataGridView1.DefaultCellStyle = dataGridViewCellStyle1;
+            dataGridView1.Location = new Point(285, 382);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.ReadOnly = true;
+            dataGridView1.Size = new Size(941, 259);
+            dataGridView1.TabIndex = 44;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Poppins", 13F);
+            label8.ForeColor = Color.FromArgb(15, 46, 103);
+            label8.Location = new Point(421, 201);
+            label8.Name = "label8";
+            label8.Size = new Size(77, 31);
+            label8.TabIndex = 45;
+            label8.Text = "display";
+            label8.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Poppins", 13F);
+            label9.ForeColor = Color.FromArgb(15, 46, 103);
+            label9.Location = new Point(421, 262);
+            label9.Name = "label9";
+            label9.Size = new Size(77, 31);
+            label9.TabIndex = 46;
+            label9.Text = "display";
+            label9.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Poppins", 13F);
+            label10.ForeColor = Color.FromArgb(15, 46, 103);
+            label10.Location = new Point(975, 201);
+            label10.Name = "label10";
+            label10.Size = new Size(77, 31);
+            label10.TabIndex = 47;
+            label10.Text = "display";
+            label10.TextAlign = ContentAlignment.TopCenter;
+            // 
             // UserMyAcc
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(238, 238, 238);
             ClientSize = new Size(1264, 681);
+            Controls.Add(label10);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(dataGridView1);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label3);
+            Controls.Add(label4);
+            Controls.Add(label1);
+            Controls.Add(label2);
             Controls.Add(headerpanel);
             Controls.Add(taskbarpanel);
             Name = "UserMyAcc";
@@ -333,7 +497,9 @@
             panel4.ResumeLayout(false);
             panel4.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -359,5 +525,16 @@
         private Panel panel4;
         private PictureBox pictureBox6;
         private Label Settingslbl;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private DataGridView dataGridView1;
+        private Label label8;
+        private Label label9;
+        private Label label10;
     }
 }
