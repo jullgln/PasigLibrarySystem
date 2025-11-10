cc33da5e1ed63b6044d43d7e1a0c582e27faa3cc
Julliane Galano <jullianegalano@gmail.com>
2025-11-10 21:22:22 +0800

    settings changed

--- USER/UserSettings.Designer.cs
+++ USER/UserSettings.Designer.cs
@@ -50,6 +50,20 @@
             highlightpanel = new Panel();
             pictureBox6 = new PictureBox();
             Settingslbl = new Label();
+            label5 = new Label();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            link2 = new LinkLabel();
+            label4 = new Label();
+            label6 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            borrowbtn = new Button();
+            button1 = new Button();
             headerpanel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             taskbarpanel.SuspendLayout();
@@ -303,11 +317,171 @@
             Settingslbl.Text = "Settings";
             Settingslbl.TextAlign = ContentAlignment.TopCenter;
             // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Poppins Medium", 12.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.ForeColor = Color.FromArgb(15, 46, 103);
+            label5.Location = new Point(284, 113);
+            label5.Name = "label5";
+            label5.Size = new Size(117, 30);
+            label5.TabIndex = 48;
+            label5.Text = "Edit Details";
+            label5.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Poppins", 12.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.FromArgb(15, 46, 103);
+            label1.Location = new Point(284, 171);
+            label1.Name = "label1";
+            label1.Size = new Size(65, 30);
+            label1.TabIndex = 49;
+            label1.Text = "Name";
+            label1.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Poppins", 12.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.FromArgb(15, 46, 103);
+            label2.Location = new Point(284, 228);
+            label2.Name = "label2";
+            label2.Size = new Size(101, 30);
+            label2.TabIndex = 50;
+            label2.Text = "Username";
+            label2.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Poppins", 12.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.ForeColor = Color.FromArgb(15, 46, 103);
+            label3.Location = new Point(284, 294);
+            label3.Name = "label3";
+            label3.Size = new Size(59, 30);
+            label3.TabIndex = 51;
+            label3.Text = "Email";
+            label3.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // link2
+            // 
+            link2.AutoSize = true;
+            link2.Font = new Font("Poppins", 12.75F);
+            link2.LinkColor = Color.FromArgb(15, 46, 103);
+            link2.Location = new Point(284, 353);
+            link2.Name = "link2";
+            link2.Size = new Size(176, 30);
+            link2.TabIndex = 52;
+            link2.TabStop = true;
+            link2.Text = "Change Password?";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Poppins", 12.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.ForeColor = Color.FromArgb(15, 46, 103);
+            label4.Location = new Point(290, 403);
+            label4.Name = "label4";
+            label4.Size = new Size(136, 30);
+            label4.TabIndex = 53;
+            label4.Text = "New Password";
+            label4.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Poppins", 12.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label6.ForeColor = Color.FromArgb(15, 46, 103);
+            label6.Location = new Point(290, 455);
+            label6.Name = "label6";
+            label6.Size = new Size(210, 30);
+            label6.TabIndex = 54;
+            label6.Text = "Confirm New Password";
+            label6.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Poppins", 12F);
+            textBox1.Location = new Point(548, 169);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(577, 31);
+            textBox1.TabIndex = 55;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Poppins", 12F);
+            textBox2.Location = new Point(548, 226);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(577, 31);
+            textBox2.TabIndex = 56;
+            // 
+            // textBox3
+            // 
+            textBox3.Font = new Font("Poppins", 12F);
+            textBox3.Location = new Point(548, 292);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(577, 31);
+            textBox3.TabIndex = 57;
+            // 
+            // textBox4
+            // 
+            textBox4.Font = new Font("Poppins", 12F);
+            textBox4.Location = new Point(548, 401);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(577, 31);
+            textBox4.TabIndex = 58;
+            // 
+            // textBox5
+            // 
+            textBox5.Font = new Font("Poppins", 12F);
+            textBox5.Location = new Point(548, 453);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(577, 31);
+            textBox5.TabIndex = 59;
+            // 
+            // borrowbtn
+            // 
+            borrowbtn.FlatStyle = FlatStyle.Flat;
+            borrowbtn.Font = new Font("Poppins Medium", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            borrowbtn.Location = new Point(1022, 529);
+            borrowbtn.Name = "borrowbtn";
+            borrowbtn.Size = new Size(103, 32);
+            borrowbtn.TabIndex = 60;
+            borrowbtn.Text = "Confirm";
+            borrowbtn.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Poppins Medium", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(871, 529);
+            button1.Name = "button1";
+            button1.Size = new Size(103, 32);
+            button1.TabIndex = 61;
+            button1.Text = "Cancel";
+            button1.UseVisualStyleBackColor = true;
+            // 
             // UserSettings
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1264, 681);
+            Controls.Add(button1);
+            Controls.Add(borrowbtn);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label6);
+            Controls.Add(label4);
+            Controls.Add(link2);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(label5);
             Controls.Add(headerpanel);
             Controls.Add(taskbarpanel);
             Name = "UserSettings";
@@ -333,6 +507,7 @@
             highlightpanel.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -358,5 +533,19 @@
         private Panel highlightpanel;
         private PictureBox pictureBox6;
         private Label Settingslbl;
+        private Label label5;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private LinkLabel link2;
+        private Label label4;
+        private Label label6;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private Button borrowbtn;
+        private Button button1;
     }
 }
